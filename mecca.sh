182e1a464684aae5d9c4169ef7defc8f0b7d1f66
Bobby Towers <porkostomus@mail.com>
2018-04-15 16:29:36 -0700

    generates half, quarter and eighth note

--- mecca.sh
+++ mecca.sh
@@ -4,6 +4,16 @@
 
 sox -n half.wav synth 0.5 tri $1 fade 0 0.7 0.25 trim 0 0.5
 
-# Play it
+# Make quarter note
 
-play out.wav
+sox half.wav quarter.wav trim 0.25 0.5
+
+# Make eighth note
+
+sox half.wav eighth.wav trim 0.37 0.5
+
+# Play them
+
+play half.wav
+play quarter.wav
+play eighth.wav

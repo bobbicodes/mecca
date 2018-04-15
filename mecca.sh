7602482d14e64684f7b129bcb37d1ec90725c373
Bobby Towers <porkostomus@mail.com>
2018-04-15 16:38:57 -0700

    cleanup

--- mecca.sh
+++ mecca.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Generate a half note at freq of arg1
+# Generate half, quarter, eighth note at freq of arg1 in Hz
+# Apply fade at note tail and trim to proper length
 
 sox -n half.wav synth 0.5 tri $1 fade 0 0.7 0.25 trim 0 0.5
-
-# Make quarter note
-
 sox half.wav quarter.wav trim 0.25 0.5
-
-# Make eighth note
-
 sox half.wav eighth.wav trim 0.37 0.5
 
 # Play them

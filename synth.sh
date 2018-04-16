5f556a3b13b162ac668f5966cfcb50bcdd927525
Bobby Towers <porkostomus@mail.com>
2018-04-15 21:11:40 -0700

    play note as background process

--- synth.sh
+++ synth.sh
@@ -19,79 +19,79 @@ while true; do
 			play e2_quarter.wav &
 			;;
 		v)
-			play f2_quarter.wav
+			play f2_quarter.wav &
 			;;
 		g)
-			play fsharp2_quarter.wav
+			play fsharp2_quarter.wav &
 			;;
 		b)
-			play g2_quarter.wav
+			play g2_quarter.wav &
 			;;
 		h)
-			play gsharp2_quarter.wav
+			play gsharp2_quarter.wav &
 			;;
 		n)
-			play a2_quarter.wav
+			play a2_quarter.wav &
 			;;
 		j)
-			play asharp2_quarter.wav
+			play asharp2_quarter.wav &
 			;;
 		m)
-			play b2_quarter.wav
+			play b2_quarter.wav &
 			;;
 		,)
-			play c3_quarter.wav
+			play c3_quarter.wav &
 			;;
 		l)
-			play csharp3_quarter.wav
+			play csharp3_quarter.wav &
 			;;
 		.)
-			play d3_quarter.wav
+			play d3_quarter.wav &
 			;;
 		\;)
-			play dsharp3_quarter.wav
+			play dsharp3_quarter.wav &
 			;;
 		/)
-			play e3_quarter.wav
+			play e3_quarter.wav &
 			;;
 		q)
-			play c3_quarter.wav
+			play c3_quarter.wav &
         ;;
 		2)
-			play csharp3_quarter.wav
+			play csharp3_quarter.wav &
   		    ;;
 		w)
-			play d3_quarter.wav
+			play d3_quarter.wav &
 	 		;;
 		3)
-			play dsharp3_quarter.wav
+			play dsharp3_quarter.wav &
   	      ;;
 		e)
-			play e3_quarter.wav
+			play e3_quarter.wav &
 		    ;;
 		r)
-			play f3_quarter.wav
+			play f3_quarter.wav &
 		    ;;
 		5)
-			play fsharp3_quarter.wav
+			play fsharp3_quarter.wav &
 			;;
 		t)
-			play g3_quarter.wav
+			play g3_quarter.wav &
   	      ;;
 		6)
-			play gsharp3_quarter.wav
+			play gsharp3_quarter.wav &
   	      ;;
 		y)
-			play a3_quarter.wav
+			play a3_quarter.wav &
   	      ;;
 		7)
-			play asharp3_quarter.wav
+			play asharp3_quarter.wav &
   	      ;;
 		u)
-			play b3_quarter.wav
+			play b3_quarter.wav &
   	      ;;
 		i)
-			play c3_quarter.wav
+			play c3_quarter.wav &
   	      ;;
 	esac
 done

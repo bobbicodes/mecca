16281bf1b7da44715632fa21946c38c777cd6abf
Bobby Towers <porkostomus@mail.com>
2018-04-17 10:19:30 -0700

    made the keys light up, cuz why not

--- synth.sh
+++ synth.sh
@@ -1,57 +1,324 @@
 #!/bin/bash
+
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+
 while true; do
-	echo "Synth ready..."
+
+# Grab single keypress from user
+	
 	read -n1 key
 	
-# Map keypresses to notes C2 - G4 following the usual convention.
-# The following calls to SoX produce a triangle wave for 0.3 seconds
-# at its proper frequency in the equal-tempered scale in Hz:
-
+# Map keypress to notes C2 - C4 following the usual convention for virtual pianos
+	
 	case $key in
-		z) play -n synth 0.3 tri 65.41 & ;;
-		s) play -n synth 0.3 tri 69.3 & ;;
-		x) play -n synth 0.3 tri 73.42 & ;;
-		d) play -n synth 0.3 tri 77.78 & ;;
-		c) play -n synth 0.3 tri 82.41 & ;;
-		v) play -n synth 0.3 tri 87.31 & ;;
-		g) play -n synth 0.3 tri 92.5 & ;;
-		b) play -n synth 0.3 tri 98 & ;;
-		h) play -n synth 0.3 tri 103.83 & ;;
-		n) play -n synth 0.3 tri 110 & ;;
-		j) play -n synth 0.3 tri 116.54 & ;;
-		m) play -n synth 0.3 tri 123.47 & ;;
-		,) play -n synth 0.3 tri 130.81 & ;;
-		l) play -n synth 0.3 tri 138.59 & ;;
-		.) play -n synth 0.3 tri 146.83 & ;;
-		\;) play -n synth 0.3 tri 155.56 & ;;
-		/) play -n synth 0.3 tri 164.81 & ;;
-		\') play -n synth 0.3 tri 174.61 & ;;
-		q) play -n synth 0.3 tri 130.81 & ;;
-		2) play -n synth 0.3 tri 138.59 & ;;
-		w) play -n synth 0.3 tri 146.83 & ;;
-		3) play -n synth 0.3 tri 155.56 & ;;
-		e) play -n synth 0.3 tri 164.81 & ;;
-		r) play -n synth 0.3 tri 174.61 & ;;
-		5) play -n synth 0.3 tri 185 & ;;
-		t) play -n synth 0.3 tri 196 & ;;
-		6) play -n synth 0.3 tri 207.65 & ;;
-		y) play -n synth 0.3 tri 220 & ;;
-		7) play -n synth 0.3 tri 233.08 & ;;
-		u) play -n synth 0.3 tri 246.94 & ;;
-		i) play -n synth 0.3 tri 261.63 & ;;
-		9) play -n synth 0.3 tri 277.18 & ;;
-		o) play -n synth 0.3 tri 293.66 & ;;
-		0) play -n synth 0.3 tri 311.13 & ;;
-		p) play -n synth 0.3 tri 329.63 & ;;
-		[) play -n synth 0.3 tri 349.23 & ;;
-		=) play -n synth 0.3 tri 369.99 & ;;
-		]) play -n synth 0.3 tri 392 & ;;
+		z) play -n synth 0.3 tri 65.41 &>/dev/null &
+			echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "\e[7mz\e[27m x c v b n m , . /"
+		 ;;
+		s) play -n synth 0.3 tri 69.3 &>/dev/null &
+			echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " \e[7ms\e[27m d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+	;;
+		x) play -n synth 0.3 tri 73.42 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z \e[7mx\e[27m c v b n m , . /"
+;;
+		d) play -n synth 0.3 tri 77.78 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s \e[7md\e[27m   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		c) play -n synth 0.3 tri 82.41 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x \e[7mc\e[27m v b n m , . /"
+ ;;
+		v) play -n synth 0.3 tri 87.31 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c \e[7mv\e[27m b n m , . /"
+;;
+		g) play -n synth 0.3 tri 92.5 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   \e[7mg\e[27m h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		b) play -n synth 0.3 tri 98 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v \e[7mb\e[27m n m , . /"
+;;
+		h) play -n synth 0.3 tri 103.83 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g \e[7mh\e[27m j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		n) play -n synth 0.3 tri 110 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v b \e[7mn\e[27m m , . /"
+;;
+		j) play -n synth 0.3 tri 116.54 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h \e[7mj\e[27m   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		m) play -n synth 0.3 tri 123.47 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v b n \e[7mm\e[27m , . /"
+;;
+		,) play -n synth 0.3 tri 130.81 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v b n m \e[7m,\e[27m . /"
+;;
+		l) play -n synth 0.3 tri 138.59 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   \e[7ml\e[27m ; '"
+	echo "z x c v b n m , . /"
+;;
+		.) play -n synth 0.3 tri 146.83 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v b n m , \e[7m.\e[27m /"
+;;
+		\;) play -n synth 0.3 tri 155.56 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l \e[7m;\e[27m '"
+	echo "z x c v b n m , . /"
+;;
+		/) play -n synth 0.3 tri 164.81 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo " s d   g h j   l ; '"
+	echo -e "z x c v b n m , . \e[7m/\e[27m"
+;;
+		\') play -n synth 0.3 tri 174.61 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; \e[7m'\e[27m"
+	echo "z x c v b n m , . /"
+;;
+		q) play -n synth 0.3 tri 130.81 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "\e[7mq\e[27m w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		2) play -n synth 0.3 tri 138.59 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  \e[7m2\e[27m 3   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		w) play -n synth 0.3 tri 146.83 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q \e[7mw\e[27m e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		3) play -n synth 0.3 tri 155.56 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 \e[7m3\e[27m   5 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		e) play -n synth 0.3 tri 164.81 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w \e[7me\e[27m r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		r) play -n synth 0.3 tri 174.61 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e \e[7mr\e[27m t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		5) play -n synth 0.3 tri 185 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   \e[7m5\e[27m 6 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		t) play -n synth 0.3 tri 196 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r \e[7mt\e[27m y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		6) play -n synth 0.3 tri 207.65 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   5 \e[7m6\e[27m 7   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		y) play -n synth 0.3 tri 220 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t \e[7my\e[27m u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		7) play -n synth 0.3 tri 233.08 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   5 6 \e[7m7\e[27m   9 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		u) play -n synth 0.3 tri 246.94 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y \e[7mu\e[27m i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		i) play -n synth 0.3 tri 261.63 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y u \e[7mi\e[27m o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		9) play -n synth 0.3 tri 277.18 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   5 6 7   \e[7m9\e[27m 0   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		o) play -n synth 0.3 tri 293.66 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y u i \e[7mo\e[27m p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		0) play -n synth 0.3 tri 311.13 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   5 6 7   9 \e[7m0\e[27m   ="
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		p) play -n synth 0.3 tri 329.63 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y u i o \e[7mp\e[27m [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		[) play -n synth 0.3 tri 349.23 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y u i o p \e[7m[\e[27m ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		=) play -n synth 0.3 tri 369.99 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo -e "  2 3   5 6 7   9 0   \e[7m=\e[27m"
+	echo "q w e r t y u i o p [ ]"
+	echo -e " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
+		]) play -n synth 0.3 tri 392 &>/dev/null &
+echo "Synth ready..."
+	echo ""
+	echo "  2 3   5 6 7   9 0   ="
+	echo -e "q w e r t y u i o p [ \e[7m]\e[27m"
+	echo " s d   g h j   l ; '"
+	echo "z x c v b n m , . /"
+;;
 	esac
 done
-
-# This creates a two octave virtual piano keyboard beginning with the z-row:
-#
-#		 2 3   5 6 7   9 0   =   
-#		q w e r t y u i o p [ ]
-#		 s d   g h j   l ; '
-#		z x c v b n m , . /
